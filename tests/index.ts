ec6a8b771d9643d6c7b75e44051ddc51bb5b6d5d
reujab <reujab@gmail.com>
2019-01-12 16:55:04 -0500

    cengage-9e -> it-guide

--- tests/index.ts
+++ tests/index.ts
@@ -1,9 +1,9 @@
-import cengage9e from "./cengage-9e"
+import itGuide from "./it-guide"
 
 export default [
 	{
-		id: "cengage-9e",
-		title: "Cengage A+: 9th Edition",
-		questions: cengage9e,
+		id: "it-guide",
+		title: "A+ Guide to IT Technical Support: 9th Edition",
+		questions: itGuide,
 	},
 ]
